e0a1b02ac80a2417a2a25944ad6a435df584a558
Tal Amit <talamit40@gmail.com>
2017-12-15 00:01:08 -0800

    init

---
+++ semantic-analyzer.scm
@@ -0,0 +1,15 @@
+(define remove-applic-lambda-nil
+  ;; fill in the empty-lambda application elimination details here
+  )
+
+(define box-set
+  ;; fill in the variable boxing details here
+  )
+
+(define pe->lex-pe
+  ;; fill in the lexical addressing details here
+  )
+
+(define annotate-tc
+  ;; fill in the tail-call annotation details here
+  )
