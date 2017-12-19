8194ed6833f9a3615e04b1bcd6c6302ceab8b3a2
Unknown <roy630@gmail.com>
2017-12-19 17:31:24 +0200

    remove-applic-lambda-nil done

--- semantic-analyzer.scm
+++ semantic-analyzer.scm
@@ -1,5 +1,69 @@
+(define isRedundantApplications
+  (lambda  (exp)
+    (let ((applic (car exp))
+          (proc   (lambda () (caadr exp)))
+          (procArg (lambda () (cadadr exp))))
+
+          (and (equal? applic 'applic) 
+               (equal? (proc) 'lambda-simple) 
+               (null? (procArg))
+          )    
+    )
+  )
+)
+
+
+
+
 (define remove-applic-lambda-nil
-  ;; fill in the empty-lambda application elimination details here
+  (lambda (parsedExp) 
+      (cond 
+        ;;in case its an empty list
+        ((null? parsedExp)  '())
+        ;;in case its a more complicated expression  
+        ((list? (car parsedExp)) 
+          (cons
+          (remove-applic-lambda-nil (car parsedExp))
+          (remove-applic-lambda-nil (cdr parsedExp))
+          ))
+        ;;in case it is an redundant Application
+        ((isRedundantApplications parsedExp)  
+          (let ((expr (car (cddadr parsedExp))))
+            (remove-applic-lambda-nil expr)
+          )
+        )
+        (else
+          (cons (car parsedExp) (remove-applic-lambda-nil (cdr parsedExp)) )
+          )
+      )
+  )
+)
+
+(define test
+  (lambda ()
+  (remove-applic-lambda-nil
+    '(applic
+    (lambda-simple
+    (fact)
+    (seq ((set (var fact) (box (var fact)))
+    (box-set
+    (var fact)
+    (lambda-simple
+    (n)
+    (if3 (applic (var zero?) ((var n)))
+    (const 1)
+    (applic
+    (var *)
+    ((var n)
+    (applic
+    (box-get (var fact))
+    ((applic (var -) ((var n) (const 1))))))))))
+    (applic
+    (lambda-simple () (applic (box-get (var fact)) ((const 5))))
+    ()))))
+    ((const #f))))
+    
+    )
   )
 
 (define box-set
@@ -13,3 +77,8 @@
 (define annotate-tc
   ;; fill in the tail-call annotation details here
   )
+
+
+ 
+    
+ 
