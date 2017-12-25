a4ed8ac477a65923479046dc0f59a1ce2702a870
Tal Amit <talamit40@gmail.com>
2017-12-25 08:51:27 -0800

    hallelujah!!! pe->lex-pe Done

--- semantic-analyzer.scm
+++ semantic-analyzer.scm
@@ -367,15 +367,165 @@
   )
 )
 
-(define pe->lex-pe
-  ;; fill in the lexical addressing details here
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;; pe->lex-pe;;;;;;;;;;;;;;;;;;;;;;
+
+(define getListOfIndexes
+  (lambda (params) 
+    (if (null? params) '()
+        (append (getListOfIndexes (cdr params))  (list (- (length params) 1)))
+    )
   )
+)
+
+;;make list of (parameter , index)
+(define updt-params-lst
+  (lambda (listVars)
+    (map (lambda (v i) (list v i)) listVars (getListOfIndexes listVars))
+  )
+)
+
+;;clear vars from scope that also in params list
+(define clearDup
+  (lambda (scope params)
+    (if (or (null? scope) (null? params))
+        scope
+       (clearDup (filter (lambda(x) (not (equal? (car x) (caar params)))) scope) (cdr params))
+    )
+  )
+)
+
+
+(define increaseBound
+  (lambda (scope)
+  (map (lambda(x) `( ,(car x) ,(+ 1 (cadr x)) ,@(cddr x))) scope)
+  )
+)
+
+(define addMajorField
+  (lambda (params)
+    (if (null? params) '()
+        (map (lambda(x) (list (car x) '-1 (cadr x))) params)
+    
+    )
+  )
+)
+
+
+;;update the scope list
+(define updt-scope-lst
+  (lambda (scope params)
+    (let* ( (scopeClearDup (clearDup scope params))
+            (nScope (increaseBound scope))
+            (nParams (addMajorField params))
+          )
+          (append nParams nScope)
+    )
+  )
+)
+
+;;check if var is in parameter list
+(define inParams?
+	(lambda (var params)
+		(let ((ret (filter (lambda (x) (equal? (car x) var)) params)))
+			(if (null? ret)
+				#f
+        (cdar ret)
+      )
+    )
+  )
+)
+
+;;check if var is in scope list          
+(define inScope?
+	(lambda (var scope)
+		(let ((ret (filter (lambda (x) (equal? (car x) var)) scope)))
+			(if (null? ret)
+				#f
+        (cdar ret)
+      )
+    )
+  )
+)
+
+
+(define auxiliary-pe->lex-pe
+  (lambda (ast scope params)
+    (cond 
+      ((null? ast) '())
+     
+      ;;case var
+      ((equal? (car ast) 'var)
+        (let (  (parameterIndex (inParams? (cadr ast) params))
+                (boundIndex (inScope? (cadr ast) scope))
+             )
+             (if parameterIndex
+                  (append `(pvar ,(cadr ast) ,(car parameterIndex) ) (auxiliary-pe->lex-pe (cddr ast) scope params))
+                  (if boundIndex
+                  (append `(bvar ,(cadr ast) ,(car boundIndex) ,(cadr boundIndex)) (auxiliary-pe->lex-pe (cddr ast) scope params)) 
+                    (append `(fvar ,(cadr ast)) (auxiliary-pe->lex-pe (cddr ast) scope params))
+                  )
+            )
+        )   
+      )      
+
+      ((eq? (car ast) 'lambda-simple) 
+        (let* ( (lamVrs (getLambdaVars 'lambda-simple ast)) ;;lambda vars
+                (lamBody (getLambdaBody 'lambda-simple ast));;lambda body
+                (newParams (updt-params-lst lamVrs));;new params list
+                (newScope (updt-scope-lst scope newParams));;new scope list
+              )
+              `(lambda-simple ,lamVrs ,(auxiliary-pe->lex-pe lamBody newScope newParams))
+        )
+      )     
+     
+      ;;opt & variadic case
+      ((eq? (car ast) 'lambda-opt)
+        (let* ( (lamVrs (getLambdaVars 'lambda-opt ast));;lambda vars
+                (lamBody (getLambdaBody 'lambda-opt ast));;lambda body
+                (newParams (updt-params-lst lamVrs));;new params list
+                (newScope (updt-scope-lst scope newParams));;new scope list
+              )
+               `(lambda-opt ,lamvars ,(auxiliary-pe->lex-pe lamBody newScope newParams))
+        ) 
+      ) 
+              
+      ;;recursive auxiliary-pe->lex-pe on car and cdr       
+      ((list? (car ast))
+         (cons (auxiliary-pe->lex-pe (car ast) scope params) (auxiliary-pe->lex-pe (cdr ast) scope params))
+      )  
+
+      ;;(car ast) is apply or somthing like that
+      (else 
+        (cons (car ast) (auxiliary-pe->lex-pe (cdr ast) scope params))
+      )   
+
+    )
+  )
+)
+
+(define pe->lex-pe
+  (lambda (parsedExp)
+    (auxiliary-pe->lex-pe parsedExp '() '())
+  )
+)
+
+;;tests for pe->lex-pe
+(define test1
+  (pe->lex-pe '(applic (var x) ((lambda-simple (x) (applic (var x) ((lambda-simple () (applic (var x) ((lambda-simple () (applic (var x) ((var x)))))))))))))
+)
+
+(define test2
+  (pe->lex-pe '(lambda-simple (a b) (lambda-simple (c) (applic (var +) ((var a) (var b) (var c))))))
+)
+
+(define test3
+  (pe->lex-pe '(define (var fact) (lambda-simple (n) (if3 (applic (var zero?) ((var n))) (const 1) (applic (var *) ((var n) (applic (var fact) ((applic (var -) ((var n) (const 1)))))))))))
+)
+
 
 (define annotate-tc
   ;; fill in the tail-call annotation details here
   )
-
-
- 
-    
- 
